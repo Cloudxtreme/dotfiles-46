43518c015b2498927c36342b44cf2eecdbd83766
Manfred Touron <m@42.am>
2016-05-11 10:07:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-svn export https://github.com/moul/junk/trunk/dotfiles
+CURDIR=$(pwd)
+
+# create symlinks
+for file in .bashrc .brew .gitconfig .ssh/.* .zshrc Library/Preferences/com.apple.Terminal.plist Vagrantfile virtualenvwrapper; do
+    (
+        if [ -e "${HOME}/${file}" ]; then
+            echo "- ln -s ${CURDIR}/${file} ${HOME}/${file}: file exists"
+        else
+            (
+                set -x
+                ln -s "${CURDIR}/${file}" "${HOME}/${file}"
+            )
+        fi
+    )
+done
+
+# configure osx prefs
+echo "You can configure OS X preferences using the following command:"
+echo "   bash -x ./.osx-prefs"
